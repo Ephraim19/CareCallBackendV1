a8591f7df735148fcb7fd3fd9cb2d165acb71b85
Ephraim19 <machayoephraim@gmail.com>
2024-06-26 13:46:02 +0300

    Cors header origin

--- Backendv1/settings.py
+++ Backendv1/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'BioData',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -34,10 +35,13 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'Backendv1.urls'
 
