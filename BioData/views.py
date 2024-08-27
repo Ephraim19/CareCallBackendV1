d360b58ef4417a2ad8ef364a8fee15d3695a5f66
Ephraim19 <machayoephraim@gmail.com>
2024-08-27 13:35:53 +0300

    Save Inbound texts

--- BioData/views.py
+++ BioData/views.py
@@ -1404,10 +1404,11 @@ def whatsapp_webhook(request):
         print(" ")
 
         member = (int(data['entry'][0]['changes'][0]['value']['contacts'][0]['wa_id'][3:]))
+        print(member)
 
         if (data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body'] is not None):
             Whatsapp.objects.create(
-            memberId = Member.objects.get(memberPhone = member),
+            memberId = Member.objects.filter(memberPhone = member),
             message = data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body'],
             messageStatus = 'received',
             messageFrom = data['entry'][0]['changes'][0]['value']['contacts'][0]['wa_id'],
