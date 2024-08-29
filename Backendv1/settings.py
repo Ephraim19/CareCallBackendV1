94e30c4c05bbe1d6054622c61b822bf5f41825a5
Ephraim19 <machayoephraim@gmail.com>
2024-08-29 12:56:28 +0300

    Mysql db

--- Backendv1/settings.py
+++ Backendv1/settings.py
@@ -88,13 +88,23 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#Mysql db
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Ephraim$Mokamba',
+        'USER': 'Ephraim',
+        'PASSWORD': 'Mokamba@17',
+        'HOST':'Ephraim.mysql.pythonanywhere-services.com',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
